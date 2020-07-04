162cc06ef37f75c54d610ac31338799ce677167e
Salsabeel Hussien <sals1995@users.noreply.github.com>
2020-07-04 08:50:15 +0300

    styling weather-card

--- weatherApp/src/app/weather/weather.data.ts
+++ weatherApp/src/app/weather/weather.data.ts
@@ -1,7 +1,7 @@
 import { WeatherItemClass } from './classes/weatherItem';
 
 export const WEATHER_ITEM : WeatherItemClass[] = [
-    // new WeatherItemClass('Giza','cloudy',10),
+    new WeatherItemClass('Giza','cloudy',10),
 ];
 
 
